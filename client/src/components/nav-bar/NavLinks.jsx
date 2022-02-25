93676f4800ff80882ef4d5afc03d700a8bac9895
Maria McBride <mariacmcbride2@gmail.com>
2022-02-25 13:40:29 -0800

    Changed Dashboard to LandingPage

--- client/src/components/nav-bar/NavLinks.jsx
+++ client/src/components/nav-bar/NavLinks.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 import * as React from 'react';
-import {  useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import Avatar from '@mui/material/Avatar';
 import { FormControl, InputLabel } from '@mui/material';
 import LoginModal from '../userModals/LoginModal'
@@ -9,8 +9,8 @@ import { MenuItem, Select } from '@mui/material';
 
 import EditModal from '../userModals/EditModal';
 import { textAlign } from '@mui/system';
-import { Dashboard } from '../../views/Dashboard';
-export default ({ logout, changeUser,loggedInUser, setChangeUser, setLoggedInUser, refresh }) => {
+import { LandingPage } from '../../views/LandingPage';
+export default ({ logout, changeUser, loggedInUser, setChangeUser, setLoggedInUser, refresh }) => {
   const history = useHistory()
   const logo = require('../static/img/logo.png')
   const buttonStyle = {
@@ -23,114 +23,114 @@ export default ({ logout, changeUser,loggedInUser, setChangeUser, setLoggedInUse
   }
   const onChangeHandler = (val) => {
     val === 'logout' ?
-    logout()
-    :
-    // val === 'editUser'?
-    
-  
-    history.push(`/${val}`)
+      logout()
+      :
+      // val === 'editUser'?
+
+
+      history.push(`/${val}`)
   }
 
   console.log(loggedInUser);
 
   return (
-    
-    <div className='nav-links'>
-   
 
-   
-        {loggedInUser == 'no user' ?
-          <ul className='navlinks d-flex pt-3 px-3'>
+    <div className='nav-links'>
+
+
+
+      {loggedInUser == 'no user' ?
+        <ul className='navlinks d-flex pt-3 px-3'>
 
 
 
           <li>
 
-          <Avatar variant="square"
-          src={logo} alt="logo"
-          sx={{
-            height: 65,
-            width: 255,
-            mb: 3,
-            pl: 3,
-            textAlign: 'center'
-          }}
-        >
-        </Avatar>
+            <Avatar variant="square"
+              src={logo} alt="logo"
+              sx={{
+                height: 65,
+                width: 255,
+                mb: 3,
+                pl: 3,
+                textAlign: 'center'
+              }}
+            >
+            </Avatar>
 
 
           </li>
 
-            <li className='list-unstyled ps-2'>
-              <LoginModal
-               loggedInUser={loggedInUser}
-                setChangeUser={setChangeUser}
-                setLoggedInUser={setLoggedInUser}
-              >
-                Log In
-              </LoginModal>
-            
-
-            </li>
-            <li className='list-unstyled ps-2'>
-            <RegistrationModal>
-                Register
-              </RegistrationModal>
-            
-              </li>
-              <li>
-
-              </li>
-          </ul>
-          :
-         
-            
-            <FormControl
-            item xs={12}
+          <li className='list-unstyled ps-2'>
+            <LoginModal
+              loggedInUser={loggedInUser}
+              setChangeUser={setChangeUser}
+              setLoggedInUser={setLoggedInUser}
             >
-              <InputLabel id="demo-simple-select-label">{loggedInUser.firstName}</InputLabel>
-              <Select
-              onChange={(e) => onChangeHandler(e.target.value)}
-                  MenuProps={{
-                    anchorOrigin: {
-                      vertical: "bottom",
-                      horizontal: "left"
-                    },
-                    transformOrigin: {
-                      vertical: "top",
-                      horizontal: "left"
-                    }
-                  }}
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                sx={{
-                  bgcolor:'white'
-                }}
-                fullWidth
-                label="Category"
-                name="category"
-                autoComplete="cuisine"
-            
-              >{
-                loggedInUser.googleUser !=1 &&
-                  <EditModal  />
+              Log In
+            </LoginModal>
+
+
+          </li>
+          <li className='list-unstyled ps-2'>
+            <RegistrationModal>
+              Register
+            </RegistrationModal>
+
+          </li>
+          <li>
+
+          </li>
+        </ul>
+        :
+
+
+        <FormControl
+          item xs={12}
+        >
+          <InputLabel id="demo-simple-select-label">{loggedInUser.firstName}</InputLabel>
+          <Select
+            onChange={(e) => onChangeHandler(e.target.value)}
+            MenuProps={{
+              anchorOrigin: {
+                vertical: "bottom",
+                horizontal: "left"
+              },
+              transformOrigin: {
+                vertical: "top",
+                horizontal: "left"
               }
-                {
-                  loggedInUser.admin == 1 &&
-                  <MenuItem value="addProduct">Add New Product</MenuItem>
-                }
-                {/* <MenuItem value="dinner">Dinner</MenuItem>
+            }}
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            sx={{
+              bgcolor: 'white'
+            }}
+            fullWidth
+            label="Category"
+            name="category"
+            autoComplete="cuisine"
+
+          >{
+              loggedInUser.googleUser != 1 &&
+              <EditModal />
+            }
+            {
+              loggedInUser.admin == 1 &&
+              <MenuItem value="addProduct">Add New Product</MenuItem>
+            }
+            {/* <MenuItem value="dinner">Dinner</MenuItem>
                 <MenuItem value="quick">Quick</MenuItem>
                 <MenuItem value="wineAndDine">Wine</MenuItem> */}
-                <MenuItem value='returns'>Returns</MenuItem>
-                <MenuItem value='logout'>Log Out</MenuItem>
-              </Select>
-            </FormControl>
-            
-          
+            <MenuItem value='returns'>Returns</MenuItem>
+            <MenuItem value='logout'>Log Out</MenuItem>
+          </Select>
+        </FormControl>
 
-        }
-       
+
+
+      }
+
     </div>
   );
 };
