03a575173dd4fa68eccb4f4e2903dc06582bc212
Matthew-Sherman1 <93622077+Matthew-Sherman1@users.noreply.github.com>
2022-02-28 14:30:33 -0700

    slider css

--- client/src/views/DashboardSections/Content.jsx
+++ client/src/views/DashboardSections/Content.jsx
@@ -8,6 +8,7 @@ import dog1 from './dog1.jpg';
 import dog2 from './dog2.jpg';
 // import dog2 from 'assets/img/splashcover.jpg';
 import './Content.css';
+
 import { FaChevronLeft, FaChevronRight } from 'react-icons'
 
 import React, { Component } from "react";
@@ -27,11 +28,13 @@ export default class SimpleSlider extends Component {
             dots: true,
             infinite: true,
             speed: 500,
-            slidesToShow: 4,
-            slidesToScroll: 4
+            slidesToShow: 5,
+            slidesToScroll: 5
         };
         return (
-            <div>
+            <div className="bg-secondary text-white slider">
+                <div className="margins">
+            
                 <div className="p-3">
                     <h3>Welcome, </h3>
                     <h2 className="p-3 m-2 text-center"> All Dogs Needing a Forerver Home</h2>
@@ -41,54 +44,54 @@ export default class SimpleSlider extends Component {
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpTmGSKs1Vrtl80YlFj1e2IcKnl9sY9catUZG9-kaPKItt10Y9glsxS4ZWvmuZrBV-DjU&usqp=CAU" />
                             <span class="imgtext">
                                 <h1>Frank</h1>
-                                <p>and this is a description</p>
+                                
                             </span>
                         </div>
                         <div class="pic">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKHqZrDyAwUD0RiOcy42xmjoh5UCz2lE6zMA&usqp=CAU" />
                             <span class="imgtext">
                                 <h1>Bilbo</h1>
-                                <p>and this is a description</p>
+                              
                             </span>
                         </div>
                         <div class="pic">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLLk2ny1Ca5FfN6UKxRy3tIl_dNq6x6HbyeA&usqp=CAU" />
                             <span class="imgtext">
                                 <h1>Adam</h1>
-                                <p>and this is a description</p>
+                                
                             </span>
                         </div>
                         <div class="pic">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx9mgwC4zmzP8-9YyLMWnRcrecE-w9grV6PQ&usqp=CAU" />
                             <span class="imgtext">
                                 <h1>Sammy</h1>
-                                <p>and this is a description</p>
+                              
                             </span>
                         </div>
                         <div class="pic">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4l57p62UtHoCex9cKr_72XorTbvrQmRhpAQ&usqp=CAU" />
                             <span class="imgtext">
                                 <h1> Name </h1>
-                                <p>and this is a description</p>
+                      
                             </span>
                         </div>
                         <div class="pic">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8QJjp0rPFttL5njt3NAsbUNi1mP2YRQtI0A&usqp=CAU" />
                             <span class="imgtext">
                                 <h1> Name </h1>
-                                <p>and this is a description</p>
+                        
                             </span>
                         </div>
                         <div class="pic">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPuYJtQznG-2YX8e7MCEnXbhI5uJYZlUD_Og&usqp=CAU" />
                             <span class="imgtext">
                                 <h1> Name </h1>
-                                <p>and this is a description</p>
+                             
                             </span>
                         </div>
                     </Slider>
                 </div>
-                <div className="p-3 m-2">
+                <div className="p-3 m-2 ">
                     <h2 className="p-3 text-center"> All Cats Needing a Forerver Home</h2>
 
                     <Slider {...settings}>
@@ -96,49 +99,49 @@ export default class SimpleSlider extends Component {
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpTmGSKs1Vrtl80YlFj1e2IcKnl9sY9catUZG9-kaPKItt10Y9glsxS4ZWvmuZrBV-DjU&usqp=CAU" />
                             <span class="imgtext">
                                 <h1>Frank</h1>
-                                <p>and this is a description</p>
+                           
                             </span>
                         </div>
                         <div class="pic">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKHqZrDyAwUD0RiOcy42xmjoh5UCz2lE6zMA&usqp=CAU" />
                             <span class="imgtext">
                                 <h1>Bilbo</h1>
-                                <p>and this is a description</p>
+                        
                             </span>
                         </div>
                         <div class="pic">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLLk2ny1Ca5FfN6UKxRy3tIl_dNq6x6HbyeA&usqp=CAU" />
                             <span class="imgtext">
                                 <h1>Adam</h1>
-                                <p>and this is a description</p>
+        
                             </span>
                         </div>
                         <div class="pic">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx9mgwC4zmzP8-9YyLMWnRcrecE-w9grV6PQ&usqp=CAU" />
                             <span class="imgtext">
                                 <h1>Sammy</h1>
-                                <p>and this is a description</p>
+                      
                             </span>
                         </div>
                         <div class="pic">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4l57p62UtHoCex9cKr_72XorTbvrQmRhpAQ&usqp=CAU" />
                             <span class="imgtext">
                                 <h1> Name </h1>
-                                <p>and this is a description</p>
+                              
                             </span>
                         </div>
                         <div class="pic">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8QJjp0rPFttL5njt3NAsbUNi1mP2YRQtI0A&usqp=CAU" />
                             <span class="imgtext">
                                 <h1> Name </h1>
-                                <p>and this is a description</p>
+                               
                             </span>
                         </div>
                         <div class="pic">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPuYJtQznG-2YX8e7MCEnXbhI5uJYZlUD_Og&usqp=CAU" />
                             <span class="imgtext">
                                 <h1> Name </h1>
-                                <p>and this is a description</p>
+                            
                             </span>
                         </div>
                     </Slider>
@@ -147,7 +150,7 @@ export default class SimpleSlider extends Component {
                     <Container>
                         <Row className='mx-auto'>
                             <Col className="mx-auto" md="8">
-                                <h2 className="title">Your Ideal Match </h2>
+                                <h2 className="title text-white">Your Ideal Match </h2>
                                 <br />
                             </Col>
                         </Row>
@@ -171,6 +174,7 @@ export default class SimpleSlider extends Component {
 
                         </Row>
                     </Container>
+                </div>
                 </div>
             </div>
         );
