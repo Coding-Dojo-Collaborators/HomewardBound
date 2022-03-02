a9e95c2a081bda40ddaabeaaf8d4f39e68f5a2a2
Eric <erictoribio2010@gmail.com>
2022-03-02 00:16:27 -0500

    api

--- client/src/views/Test.jsx
+++ client/src/views/Test.jsx
@@ -1,12 +1,15 @@
 
 import axios from 'axios';
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
 import LoginModal from '../components/Modals/LoginModal'
 
 const Test = ({loggedInUser}) => {
   const petFinderKey = process.env.REACT_APP_PET_API_Key 
   const petFinderSecret = process.env.REACT_APP_PET_API_Secret
+  const [dogs, setDogs] = useState()
+  const [cats, setCats] = useState()
   useEffect(async () => {
+   
     let gettingToken = `grant_type=client_credentials&client_id=${petFinderKey}&client_secret=${petFinderSecret}`
     axios.post(`https://api.petfinder.com/v2/oauth2/token`, gettingToken)
     .then(accessToken => {
@@ -18,18 +21,41 @@ const Test = ({loggedInUser}) => {
             url: "https://api.petfinder.com/v2/animals?type=dog&limit=100"
         }
         // console.log('this is the animals called', options)---gives me an access token
-    axios(options)
-    .then((response) => {
-        let animals = response.data.animals
+         axios(options)
+    .then(async ( response) =>  {await
+        setDogs(response.data.animals)
             // res.render('animalsIndex', {animals: animals})  
-            console.log(animals)
+            console.log(dogs)
+        
+        })
+    })
+    
+    .catch(error => {
+        console.log(error)
+    })
+  
+     axios.post(`https://api.petfinder.com/v2/oauth2/token`, gettingToken)
+    .then(accessToken => {
+        console.log('looking to see wtf is going on')
+        const header = "Bearer " + accessToken.data.access_token;
+        const options = {
+            method: 'GET',
+            headers: {'Authorization': header},
+            url: "https://api.petfinder.com/v2/animals?type=cat&limit=100"
+        }
+        // console.log('this is the animals called', options)---gives me an access token
+         axios(options)
+      .then(async (response) => {
+        await  setCats(response.data.animals)
+            // res.render('animalsIndex', {animals: animals})  
+            console.log(cats)
         
         })
     })
     .catch(error => {
         console.log(error)
     })
-  }, []);
+  }, [dogs==undefined,cats==undefined]);
   console.log(loggedInUser)
   return (
     <div>
