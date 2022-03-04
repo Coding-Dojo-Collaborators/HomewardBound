f02d9d550fe0a8742c3dc0a0aa838e013dbf4b09
Matthew-Sherman1 <93622077+Matthew-Sherman1@users.noreply.github.com>
2022-03-03 17:52:05 -0700

    dog/cat profile

--- client/src/views/DashboardSections/Quizzes/CatProfile.jsx
+++ client/src/views/DashboardSections/Quizzes/CatProfile.jsx
@@ -1,9 +1,223 @@
-import React from 'react'
-import "./profile.css"
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Nav } from 'reactstrap';
+import "./DogProfile.css"
+
+// Reactstrap components
+import {
+  List,
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Row,
+  Col,
+} from 'reactstrap';
+
+// MUI
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import PetsIcon from '@mui/icons-material/Pets';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import MoreTimeIcon from '@mui/icons-material/MoreTime';
+import PaidIcon from '@mui/icons-material/Paid';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { styled } from '@mui/material/styles';
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+// import { createTheme } from '@mui/material/styles';
+
+// Styles
+import styles from '../../../assets/css/modules/paper-dashboard.module.css';
+
+
+
 
 const CatProfile = () => {
+  const [value, setValue] = useState(1);
+  // const [activeTab, setActiveTab] = useState('radio1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const divContent = {
+    width: '80%',
+    margin: '50px auto',
+  }
+  const ListItems = styled(List)({
+    display: 'block',
+    textIndent: '-1.5px',
+    paddingLeft: '45px',
+    fontSize: '15px',
+    lineHeight: '1.5em',
+    fontWeight: '300 !important',
+  })
+  const PawIcon = styled(PetsIcon)({
+    fontSize: '14px',
+    margin: '0 10px 5px -27px'
+  })
+  const ColoredCheckbox = styled(Checkbox)({
+    color: '#2c2c2c',
+    '&.Mui-checked': {
+      color: '#f47660',
+    },
+    fontSize: '15px',
+    lineHeight: '1.5em',
+    fontWeight: '300 !important',
+    margin: '0 0 0 19px'
+  })
+  const CheckboxLabel = styled(FormControlLabel)({
+    fontSize: '15px',
+    lineHeight: '1.5em',
+    fontWeight: '300 !important',
+  })
+
   return (
-    <div>CatProfile</div>
+    <>
+      <div className={`${styles.content}`} style={divContent}>
+        <Card>
+          <CardHeader>
+            <CardTitle tag='h2' className='text-center p-4'>Cats up for Adoption</CardTitle>
+            <p className='card-category px-4'>
+              Browse our newest cats looking for a new loving home!
+            </p>
+          </CardHeader>
+          <CardBody>
+            <Card>
+              <CardBody>
+                <div className='places-buttons'>
+                  <Row>
+                    <Col className='ml-auto mr-auto text-center mb-3 mt-2' md='12'>
+                      <CardTitle tag='h4'></CardTitle>
+                    </Col>
+                  </Row>
+                  <Row className='p-3 px-5'>   
+<div class="col-md-3 col-sm-6">
+                <div class="our-team">
+                    <div class="pic">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo3EvtoELv_W5IoiYUsPvIpt7u8B-atj3PCA&usqp=CAU" />
+                    </div>
+                    <h3 class="title">Luna</h3>
+                    <span class="post">Abyssinian Cat</span>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="our-team">
+                    <div class="pic">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo3EvtoELv_W5IoiYUsPvIpt7u8B-atj3PCA&usqp=CAU" />
+                    </div>
+                    <h3 class="title">Milo</h3>
+                    <span class="post">American Bobtail Cat Breed</span>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="our-team">
+                    <div class="pic">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo3EvtoELv_W5IoiYUsPvIpt7u8B-atj3PCA&usqp=CAU" />
+                    </div>
+                    <h3 class="title">Oliver</h3>
+                    <span class="post">American Curl Cat Breed</span>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="our-team">
+                    <div class="pic">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo3EvtoELv_W5IoiYUsPvIpt7u8B-atj3PCA&usqp=CAU" />
+                    </div>
+                    <h3 class="title">Leo</h3>
+                    <span class="post">American Shorthair Cat</span>
+                </div>
+            </div>
+                  </Row>
+                  <Row className='p-3 px-5'>
+                    <div class="col-md-3 col-sm-6">
+                                    <div class="our-team">
+                                        <div class="pic">
+                                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo3EvtoELv_W5IoiYUsPvIpt7u8B-atj3PCA&usqp=CAU" />
+                                        </div>
+                                        <h3 class="title">Loki</h3>
+                                        <span class="post">Australian Mist</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-3 col-sm-6">
+                                    <div class="our-team">
+                                        <div class="pic">
+                                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo3EvtoELv_W5IoiYUsPvIpt7u8B-atj3PCA&usqp=CAU" />
+                                        </div>
+                                        <h3 class="title">Bella</h3>
+                                        <span class="post">Bengal</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-3 col-sm-6">
+                                    <div class="our-team">
+                                        <div class="pic">
+                                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo3EvtoELv_W5IoiYUsPvIpt7u8B-atj3PCA&usqp=CAU" />
+                                        </div>
+                                        <h3 class="title">Charlie</h3>
+                                        <span class="post">Bombay</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-3 col-sm-6">
+                                    <div class="our-team">
+                                        <div class="pic">
+                                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo3EvtoELv_W5IoiYUsPvIpt7u8B-atj3PCA&usqp=CAU" />
+                                        </div>
+                                        <h3 class="title">Willow</h3>
+                                        <span class="post">British Shorthair</span>
+                                    </div>
+                                </div>
+                                      </Row>
+
+                                      <Row className='p-3 px-5'>  
+                    <div class="col-md-3 col-sm-6">
+                                    <div class="our-team">
+                                        <div class="pic">
+                                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo3EvtoELv_W5IoiYUsPvIpt7u8B-atj3PCA&usqp=CAU" />
+                                        </div>
+                                        <h3 class="title">Lucy</h3>
+                                        <span class="post">Burmese</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-3 col-sm-6">
+                                    <div class="our-team">
+                                        <div class="pic">
+                                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo3EvtoELv_W5IoiYUsPvIpt7u8B-atj3PCA&usqp=CAU" />
+                                        </div>
+                                        <h3 class="title">Simba</h3>
+                                        <span class="post">Cyprus</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-3 col-sm-6">
+                                    <div class="our-team">
+                                        <div class="pic">
+                                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo3EvtoELv_W5IoiYUsPvIpt7u8B-atj3PCA&usqp=CAU" />
+                                        </div>
+                                        <h3 class="title">Evie</h3>
+                                        <span class="post">Egyptian Mau</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-3 col-sm-6">
+                                    <div class="our-team">
+                                        <div class="pic">
+                                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo3EvtoELv_W5IoiYUsPvIpt7u8B-atj3PCA&usqp=CAU" />
+                                        </div>
+                                        <h3 class="title">Craig</h3>
+                                        <span class="post">Hairless Cat</span>
+                                    </div>
+                                </div>
+                                      </Row>
+                </div>
+              </CardBody>
+            </Card>
+          </CardBody>
+        </Card>
+      </div>
+
+    </>
   )
 }
 
--- client/src/views/DashboardSections/Quizzes/DogProfile.jsx
+++ client/src/views/DashboardSections/Quizzes/DogProfile.jsx
@@ -1,9 +1,226 @@
-import React from 'react'
-import "./profile.css"
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Nav } from 'reactstrap';
+import "./DogProfile.css"
+
+// Reactstrap components
+import {
+  List,
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Row,
+  Col,
+} from 'reactstrap';
+
+// MUI
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import PetsIcon from '@mui/icons-material/Pets';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import MoreTimeIcon from '@mui/icons-material/MoreTime';
+import PaidIcon from '@mui/icons-material/Paid';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { styled } from '@mui/material/styles';
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+// import { createTheme } from '@mui/material/styles';
+
+// Styles
+import styles from '../../../assets/css/modules/paper-dashboard.module.css';
+
+
+
 
 const DogProfile = () => {
+  const [value, setValue] = useState(1);
+  // const [activeTab, setActiveTab] = useState('radio1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const divContent = {
+    width: '80%',
+    margin: '50px auto',
+  }
+  const ListItems = styled(List)({
+    display: 'block',
+    textIndent: '-1.5px',
+    paddingLeft: '45px',
+    fontSize: '15px',
+    lineHeight: '1.5em',
+    fontWeight: '300 !important',
+  })
+  const PawIcon = styled(PetsIcon)({
+    fontSize: '14px',
+    margin: '0 10px 5px -27px'
+  })
+  const ColoredCheckbox = styled(Checkbox)({
+    color: '#2c2c2c',
+    '&.Mui-checked': {
+      color: '#f47660',
+    },
+    fontSize: '15px',
+    lineHeight: '1.5em',
+    fontWeight: '300 !important',
+    margin: '0 0 0 19px'
+  })
+  const CheckboxLabel = styled(FormControlLabel)({
+    fontSize: '15px',
+    lineHeight: '1.5em',
+    fontWeight: '300 !important',
+  })
+
   return (
-    <div>DogProfile</div>
+    <>
+      <div className={`${styles.content}`} style={divContent}>
+        <Card>
+          <CardHeader>
+            <CardTitle tag='h2' className='text-center p-4'>Dogs up for Adoption</CardTitle>
+            <p className='card-category px-4'>
+              Browse our newest dogs looking for a new loving home!
+            </p>
+          </CardHeader>
+          <CardBody>
+            <Card>
+              <CardBody>
+                <div className='places-buttons'>
+                  <Row>
+                    <Col className='ml-auto mr-auto text-center mb-3 mt-2' md='12'>
+                      <CardTitle tag='h4'></CardTitle>
+                    </Col>
+                  </Row>
+                  <Row className='p-3 px-5'>
+                    <div class="col-md-3 col-sm-6">
+                      <div class="our-team">
+                        <div class="pic">
+                          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy-rjRO4reDCYkdHGLEnoDPacstC_3Zc-KXw&usqp=CAU" />
+                          <div class="overlay1">
+                            <div class="text1">Hello World</div>
+                          </div>
+                        </div>
+                        <h3 class="title">David</h3>
+                        <span class="post">Pitbull</span>
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                      <div class="our-team">
+                        <div class="pic">
+                          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy-rjRO4reDCYkdHGLEnoDPacstC_3Zc-KXw&usqp=CAU" />
+                        </div>
+                        <h3 class="title">Scruffy</h3>
+                        <span class="post">Bulldog</span>
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                      <div class="our-team">
+                        <div class="pic">
+                          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy-rjRO4reDCYkdHGLEnoDPacstC_3Zc-KXw&usqp=CAU" />
+                        </div>
+                        <h3 class="title">Sam</h3>
+                        <span class="post">Husky</span>
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                      <div class="our-team">
+                        <div class="pic">
+                          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy-rjRO4reDCYkdHGLEnoDPacstC_3Zc-KXw&usqp=CAU" />
+                        </div>
+                        <h3 class="title">Bilbo</h3>
+                        <span class="post">Weiner Dog</span>
+                      </div>
+                    </div>
+                  </Row>
+                  <Row className='p-3 px-5'>
+                    <div class="col-md-3 col-sm-6">
+                      <div class="our-team">
+                        <div class="pic">
+                          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy-rjRO4reDCYkdHGLEnoDPacstC_3Zc-KXw&usqp=CAU" />
+                        </div>
+                        <h3 class="title">Buddy</h3>
+                        <span class="post">Black Lab</span>
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                      <div class="our-team">
+                        <div class="pic">
+                          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy-rjRO4reDCYkdHGLEnoDPacstC_3Zc-KXw&usqp=CAU" />
+                        </div>
+                        <h3 class="title">Lucky</h3>
+                        <span class="post">Golden Retriever</span>
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                      <div class="our-team">
+                        <div class="pic">
+                          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy-rjRO4reDCYkdHGLEnoDPacstC_3Zc-KXw&usqp=CAU" />
+                        </div>
+                        <h3 class="title">Craig</h3>
+                        <span class="post">Chichahua</span>
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                      <div class="our-team">
+                        <div class="pic">
+                          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy-rjRO4reDCYkdHGLEnoDPacstC_3Zc-KXw&usqp=CAU" />
+                        </div>
+                        <h3 class="title">Goose</h3>
+                        <span class="post">Golden Retrevier</span>
+                      </div>
+                    </div>
+                  </Row>
+
+                  <Row className='p-3 px-5'>
+                    <div class="col-md-3 col-sm-6">
+                      <div class="our-team">
+                        <div class="pic">
+                          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy-rjRO4reDCYkdHGLEnoDPacstC_3Zc-KXw&usqp=CAU" />
+                        </div>
+                        <h3 class="title">Frank</h3>
+                        <span class="post">Poodle</span>
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                      <div class="our-team">
+                        <div class="pic">
+                          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy-rjRO4reDCYkdHGLEnoDPacstC_3Zc-KXw&usqp=CAU" />
+                        </div>
+                        <h3 class="title">Sammy</h3>
+                        <span class="post">Shih Tzu</span>
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                      <div class="our-team">
+                        <div class="pic">
+                          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy-rjRO4reDCYkdHGLEnoDPacstC_3Zc-KXw&usqp=CAU" />
+                        </div>
+                        <h3 class="title">Fluffy</h3>
+                        <span class="post">Bernese Mountain Dog</span>
+                      </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                      <div class="our-team">
+                        <div class="pic">
+                          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy-rjRO4reDCYkdHGLEnoDPacstC_3Zc-KXw&usqp=CAU" />
+                        </div>
+                        <h3 class="title">blu</h3>
+                        <span class="post">Border Collie</span>
+                      </div>
+                    </div>
+                  </Row>
+                </div>
+              </CardBody>
+            </Card>
+          </CardBody>
+        </Card>
+      </div>
+
+    </>
   )
 }
 
